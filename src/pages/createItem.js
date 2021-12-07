8b02d1e9c357b9fb644a19a1c7525220e75bfa78
Lautaro Di Sanza <lautarod@gmail.com>
2021-12-07 16:48:27 -0300

    Change pages
    createItem => Now the user can create the nft from an image or multiple.

--- src/pages/createItem.js
+++ src/pages/createItem.js
@@ -9,6 +9,7 @@ import clientIpfs from "../auth/ipfs";
 import backgroundImage from "../assets/background.jpg";
 import NFT from "../artifacts/contracts/NFT.sol/NFT.json";
 import Market from "../artifacts/contracts/Market.sol/NFTMarket.json";
+import mergeImages from "merge-images";
 
 const CreateItem = () => {
   //const client = ipfsHttpClient("https://ipfs.infura.io:5001/api/v0");
@@ -19,6 +20,8 @@ const CreateItem = () => {
     name: "",
     description: "",
   });
+  const [imagesArray, setImageArray] = useState([]);
+  const [imageInput, setImageInput] = useState(true);
   const { name, description, price } = formInput;
   async function onChange(e) {
     const file = e.target.files[0];
@@ -33,9 +36,31 @@ const CreateItem = () => {
       console.log("Error uploading file: ", error);
     }
   }
+
+  const mergeImage = () => {
+    mergeImages([
+      `${imagesArray[0]}`,
+      `${imagesArray[1]}`,
+      `${imagesArray[2]}`,
+      `${imagesArray[3]}`,
+    ]).then((b64) => setFileUrl(b64));
+  };
+
+  function getBase64(e) {
+    const file = e.target.files[0];
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function () {
+      setImageArray((imagesArray) => [...imagesArray, reader.result]);
+    };
+    reader.onerror = function (error) {
+      console.log("Error: ", error);
+    };
+  }
+
   async function createMarket() {
     const { name, description, price } = formInput;
-    console.log(name, description, price, fileUrl);
+    //console.log(name, description, price, fileUrl);
     if (!name || !description || !price || !fileUrl) return;
 
     /* first, upload to IPFS */
@@ -125,15 +150,124 @@ const CreateItem = () => {
               updateFormInput({ ...formInput, price: e.target.value })
             }
           ></InputMask>
-          <input
-            type="file"
-            name="Asset"
-            className="my-4"
-            onChange={onChange}
-          />
-          {fileUrl && (
-            <img className="rounded mt-4" width="350" src={fileUrl} />
+
+          <div
+            className="mt-2 border rounded p-4"
+            style={{ backgroundColor: "white" }}
+          >
+            <div>
+              <label className="inline-flex items-center">
+                <input
+                  type="radio"
+                  className="form-radio"
+                  name="radio"
+                  value="1"
+                  onChange={() => setImageInput(true)}
+                />
+                <span className="ml-2">Single image</span>
+              </label>
+            </div>
+            <div>
+              <label className="inline-flex items-center">
+                <input
+                  type="radio"
+                  onChange={() => setImageInput(false)}
+                  className="form-radio"
+                  name="radio"
+                  value="2"
+                />
+                <span className="ml-2">Create a avatar</span>
+              </label>
+            </div>
+          </div>
+
+          {imageInput ? (
+            <div
+              className="mt-2 border rounded p-4"
+              style={{ backgroundColor: "white" }}
+            >
+              <p>Select the image</p>
+              <input
+                type="file"
+                name="Asset"
+                className="my-4"
+                onChange={onChange}
+              />
+              <br></br>
+            </div>
+          ) : (
+            <div className="flex flex-wrap">
+              <div
+                className="mt-2 border rounded p-4"
+                style={{ backgroundColor: "white" }}
+              >
+                <p>Hair</p>
+                <input
+                  type="file"
+                  name="Asset"
+                  className="my-4"
+                  onChange={getBase64}
+                />
+                <br></br>
+              </div>
+              <div
+                className="mt-2 border rounded p-4"
+                style={{ backgroundColor: "white" }}
+              >
+                <p>Face</p>
+                <input
+                  type="file"
+                  name="Asset"
+                  className="my-4"
+                  onChange={getBase64}
+                />
+                <br></br>
+              </div>
+              <div
+                className="mt-2 border rounded p-4"
+                style={{ backgroundColor: "white" }}
+              >
+                <p>Eyes</p>
+                <input
+                  type="file"
+                  name="Asset"
+                  className="my-4"
+                  onChange={getBase64}
+                />
+                <br></br>
+              </div>
+              <div
+                className="mt-2 border rounded p-4"
+                style={{ backgroundColor: "white" }}
+              >
+                <p>Mouth</p>
+                <input
+                  type="file"
+                  name="Asset"
+                  className="my-4"
+                  onChange={getBase64}
+                />
+              </div>
+            </div>
           )}
+
+          <br></br>
+          {!imageInput ? (
+            <button
+              onClick={() => mergeImage()}
+              className="font-bold mt-4 bg-indigo-500 text-white rounded p-4 shadow-lg"
+            >
+              Merge images
+            </button>
+          ) : null}
+
+          <br></br>
+
+          {fileUrl ? (
+            <div className="w-1/2 flex flex-col pb-12">
+              <img className="rounded mt-4" width="350" src={fileUrl} />
+            </div>
+          ) : null}
           <button
             onClick={() => createMarket()}
             className="font-bold mt-4 bg-indigo-500 text-white rounded p-4 shadow-lg"
